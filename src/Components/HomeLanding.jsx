3a22601916a5cfaa9ff6f1fd65f39114d507e0d4
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-07-11 09:31:56 +0200

    adding full screen height and filling landing

--- src/Components/HomeLanding.jsx
+++ src/Components/HomeLanding.jsx
@@ -5,7 +5,7 @@ import { Recetas } from "./Recetas"
 export const HomeLanding = () => {
  
   return (
-    <div className='bg-gradient-to-b mx-auto from-white to-gray-200 max-w-screen-xl'>
+    <div className='bg-gradient-to-b mx-auto from-white to-gray-200 max-w-screen-xl min-h-screen'>
           <Recetas 
             url = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian'          
           />
